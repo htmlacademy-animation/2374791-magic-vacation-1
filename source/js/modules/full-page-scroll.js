4d40606c9a841d12171e887826d8b07a841580cb
Stepan Paliy <palstepweb@gmail.com>
2024-03-03 19:25:12 +0500

    module 6 task 3-пока так вывел фигуры, позже сформирую картину

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -4,7 +4,7 @@ import timerStart from './game-timer';
 import NumberUpAnimation from './number-up-animation';
 import {plainMeshController} from './3d-animation/plainMeshController';
 import {scene} from './3d-animation/initAnimationScreen';
-import {sphere} from './3d-animation/sphere';
+// import {sphere} from './3d-animation/sphere';
 import {sceneController} from './3d-animation/sceneController';
 
 export default class FullPageScroll {
@@ -123,10 +123,10 @@ export default class FullPageScroll {
     scene.clearScene();
 
     if (nextActiveScreen.classList.contains(`screen--intro`)) {
-      sphere.addScreenMesh(`intro`);
+      sceneController.addScreenMesh();
     } else if (nextActiveScreen.classList.contains(`screen--story`)) {
       plainMeshController.addScreenMesh(`story`).then(() => {
-        sceneController.addScreenMesh();
+        plainMeshController.setStoryActiveMesh();
       });
     }
 
