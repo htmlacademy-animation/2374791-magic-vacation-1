1c4e420194ca08e5f3c990f2ad145b604d52e7a4
Stepan Paliy <palstepweb@gmail.com>
2024-03-09 17:57:15 +0500

    module 6 task 6

--- source/js/modules/3d-animation/sceneController.js
+++ source/js/modules/3d-animation/sceneController.js
@@ -4,9 +4,12 @@ import {Saturn} from './3d-objects/saturn';
 import {Pyramid} from './3d-objects/pyramid';
 import {Snowman} from './3d-objects/snowman';
 import {Lantern} from './3d-objects/lantern';
+import {Carpet} from './3d-objects/carpet';
+import {Road} from './3d-objects/road';
 import SvgLoader from './3d-objects/intro-screen';
 import {reflection3D} from '../../helpers/3d-data';
 import {color3D} from '../../helpers/3d-data';
+
 
 export const sceneController = {
   clearScene() {
@@ -126,6 +129,25 @@ export const sceneController = {
     flower.rotation.copy(new THREE.Euler(THREE.MathUtils.degToRad(-40.0), THREE.MathUtils.degToRad(15.0), THREE.MathUtils.degToRad(20.0)), `XYZ`);
     scene.addSceneObject(flower);
   },
+  addCarpet() {
+    const carpet = new Carpet({
+      mainColor: color3D.LightPurple,
+      additionalColor: color3D.AdditionalPurple,
+    });
+    const scale = 0.7;
+    carpet.scale.set(scale, scale, scale);
+    carpet.position.set(25, 215, 20);
+    carpet.rotation.copy(new THREE.Euler(THREE.MathUtils.degToRad(13.0), THREE.MathUtils.degToRad(-52.0), 0), `XYZ`);
+    scene.addSceneObject(carpet);
+  },
+  addRoad() {
+    const road = new Road({
+      metalness: reflection3D.soft.metalness,
+      roughness: reflection3D.soft.roughness,
+    });
+    road.rotation.copy(new THREE.Euler(0, THREE.MathUtils.degToRad(-46.0), 0), `XYZ`);
+    scene.addSceneObject(road);
+  },
 
   async addScreenMesh() {
     this.addSaturn();
@@ -139,6 +161,9 @@ export const sceneController = {
     this.addSnowFlake();
     this.addLeaf();
     this.addFlowers();
+    this.addCarpet();
+    this.addRoad();
+
   },
 
 };
